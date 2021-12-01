859719ab8e5ea38910978b12806b6e24564e48e4
fuaberu <kevinfabel@gmail.com>
2021-12-01 19:57:42 -0300

    news baseline

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { cryptosApi } from '../services/cryptoApi';
-import { newsApi } from '../services/newsApi';
+// import { newsApi } from '../services/newsApi';
 
 export const store = configureStore({
 	reducer: {
